67160a0d740518ae79dd62e87c2022813fc41d46
Matias Grioni <matgrioni@gmail.com>
2018-06-07 16:41:56 -0400

    Formatting.

--- tests/unit/test_sentence.py
+++ tests/unit/test_sentence.py
@@ -67,6 +67,7 @@ def test_metadata_parsing():
     assert sentence.meta('text_en') == 'But how is it done ?'
     assert sentence.meta('translit') == 'tat yathānuśrūyate.'
 
+
 def test_singleton_parsing():
     """
     Test if the sentence can accurately parse all metadata in the comments.
@@ -88,6 +89,7 @@ def test_singleton_parsing():
     assert sentence.meta('text_en') == 'But how is it done ?'
     assert sentence.meta('translit') == 'tat yathānuśrūyate.'
 
+
 def test_id_updating():
     """
     Test updating the sentence id.
@@ -106,50 +108,54 @@ def test_id_updating():
     sentence.id = 'fr-ud-train_00123'
     assert sentence.meta('sent_id') == 'fr-ud-train_00123'
 
+
 def test_output():
     """
     Test if the sentence output is propertly produced.
     """
-    source = ('# sent_id = fr-ud-dev_00002\n'
-              '# text = Les études durent six ans mais leur contenu diffère donc selon les Facultés.\n'
-              '1	Les	le	DET	_	Definite=Def|Gender=Fem|Number=Plur|PronType=Art	2	det	_	_\n'
-              '2	études	étude	NOUN	_	Gender=Fem|Number=Plur	3	nsubj	_	_\n'
-              '3	durent	durer	VERB	_	Mood=Ind|Number=Plur|Person=3|Tense=Pres|VerbForm=Fin	0	root	_	_\n'
-              '4	six	six	NUM	_	_	5	nummod	_	_\n'
-              '5	ans	an	NOUN	_	Gender=Masc|Number=Plur	3	obj	_	_\n'
-              '6	mais	mais	CCONJ	_	_	9	cc	_	_\n'
-              '7	leur	son	DET	_	Gender=Masc|Number=Sing|Poss=Yes|PronType=Prs	8	det	_	_\n'
-              '8	contenu	contenu	NOUN	_	Gender=Masc|Number=Sing	9	nsubj	_	_\n'
-              '9	diffère	différer	VERB	_	Mood=Ind|Number=Sing|Person=3|Tense=Pres|VerbForm=Fin	3	conj	_	_\n'
-              '10	donc	donc	ADV	_	_	9	advmod	_	_\n'
-              '11	selon	selon	ADP	_	_	13	case	_	_\n'
-              '12	les	le	DET	_	Definite=Def|Number=Plur|PronType=Art	13	det	_	_\n'
-              '13	Facultés	Facultés	PROPN	_	_	9	obl	_	SpaceAfter=No\n'
-              '14	.	.	PUNCT	_	_	3	punct	_	_')
+    source = (
+        '# sent_id = fr-ud-dev_00002\n'
+        '# text = Les études durent six ans mais leur contenu diffère donc selon les Facultés.\n'
+        '1	Les	le	DET	_	Definite=Def|Gender=Fem|Number=Plur|PronType=Art	2	det	_	_\n'
+        '2	études	étude	NOUN	_	Gender=Fem|Number=Plur	3	nsubj	_	_\n'
+        '3	durent	durer	VERB	_	Mood=Ind|Number=Plur|Person=3|Tense=Pres|VerbForm=Fin	0	root	_	_\n'
+        '4	six	six	NUM	_	_	5	nummod	_	_\n'
+        '5	ans	an	NOUN	_	Gender=Masc|Number=Plur	3	obj	_	_\n'
+        '6	mais	mais	CCONJ	_	_	9	cc	_	_\n'
+        '7	leur	son	DET	_	Gender=Masc|Number=Sing|Poss=Yes|PronType=Prs	8	det	_	_\n'
+        '8	contenu	contenu	NOUN	_	Gender=Masc|Number=Sing	9	nsubj	_	_\n'
+        '9	diffère	différer	VERB	_	Mood=Ind|Number=Sing|Person=3|Tense=Pres|VerbForm=Fin	3	conj	_	_\n'
+        '10	donc	donc	ADV	_	_	9	advmod	_	_\n'
+        '11	selon	selon	ADP	_	_	13	case	_	_\n'
+        '12	les	le	DET	_	Definite=Def|Number=Plur|PronType=Art	13	det	_	_\n'
+        '13	Facultés	Facultés	PROPN	_	_	9	obl	_	SpaceAfter=No\n'
+        '14	.	.	PUNCT	_	_	3	punct	_	_')
     sentence = Sentence(source)
 
     assert sentence.conllu() == source
+
 
 def test_modified_output():
     """
     Test if the sentence is properly outputted after changing the annotation.
     """
-    source = ('# sent_id = fr-ud-dev_00002\n'
-              '# text = Les études durent six ans mais leur contenu diffère donc selon les Facultés.\n'
-              '1	Les	le	DET	_	Definite=Def|Gender=Fem|Number=Plur|PronType=Art	2	det	_	_\n'
-              '2	études	étude	NOUN	_	Gender=Fem|Number=Plur	3	nsubj	_	_\n'
-              '3	durent	durer	VERB	_	Mood=Ind|Number=Plur|Person=3|Tense=Pres|VerbForm=Fin	0	root	_	_\n'
-              '4	six	six	NUM	_	_	5	nummod	_	_\n'
-              '5	ans	an	NOUN	_	Gender=Masc|Number=Plur	3	obj	_	_\n'
-              '6	mais	mais	CCONJ	_	_	9	cc	_	_\n'
-              '7	leur	son	DET	_	Gender=Masc|Number=Sing|Poss=Yes|PronType=Prs	8	det	_	_\n'
-              '8	contenu	contenu	NOUN	_	Gender=Masc|Number=Sing	9	nsubj	_	_\n'
-              '9	diffère	différer	VERB	_	Mood=Ind|Number=Sing|Person=3|Tense=Pres|VerbForm=Fin	3	conj	_	_\n'
-              '10	donc	donc	ADV	_	_	9	advmod	_	_\n'
-              '11	selon	selon	ADP	_	_	13	case	_	_\n'
-              '12	les	le	DET	_	Definite=Def|Number=Plur|PronType=Art	13	det	_	_\n'
-              '13	Facultés	Facultés	PROPN	_	_	9	obl	_	SpaceAfter=No\n'
-              '14	.	.	PUNCT	_	_	3	punct	_	_')
+    source = (
+        '# sent_id = fr-ud-dev_00002\n'
+        '# text = Les études durent six ans mais leur contenu diffère donc selon les Facultés.\n'
+        '1	Les	le	DET	_	Definite=Def|Gender=Fem|Number=Plur|PronType=Art	2	det	_	_\n'
+        '2	études	étude	NOUN	_	Gender=Fem|Number=Plur	3	nsubj	_	_\n'
+        '3	durent	durer	VERB	_	Mood=Ind|Number=Plur|Person=3|Tense=Pres|VerbForm=Fin	0	root	_	_\n'
+        '4	six	six	NUM	_	_	5	nummod	_	_\n'
+        '5	ans	an	NOUN	_	Gender=Masc|Number=Plur	3	obj	_	_\n'
+        '6	mais	mais	CCONJ	_	_	9	cc	_	_\n'
+        '7	leur	son	DET	_	Gender=Masc|Number=Sing|Poss=Yes|PronType=Prs	8	det	_	_\n'
+        '8	contenu	contenu	NOUN	_	Gender=Masc|Number=Sing	9	nsubj	_	_\n'
+        '9	diffère	différer	VERB	_	Mood=Ind|Number=Sing|Person=3|Tense=Pres|VerbForm=Fin	3	conj	_	_\n'
+        '10	donc	donc	ADV	_	_	9	advmod	_	_\n'
+        '11	selon	selon	ADP	_	_	13	case	_	_\n'
+        '12	les	le	DET	_	Definite=Def|Number=Plur|PronType=Art	13	det	_	_\n'
+        '13	Facultés	Facultés	PROPN	_	_	9	obl	_	SpaceAfter=No\n'
+        '14	.	.	PUNCT	_	_	3	punct	_	_')
     sentence = Sentence(source)
 
     sentence.id = 'fr-ud-dev_00231'
@@ -159,22 +165,22 @@ def test_modified_output():
     sentence['13'].feats['Number'] = set()
     sentence['13'].feats['Number'].add('Fem')
 
+    output = (
+        '# sent_id = fr-ud-dev_00231\n'
+        '# text = Les études durent six ans mais leur contenu diffère donc selon les Facultés.\n'
+        '1	Les	le	DET	_	Definite=Def|Gender=Fem|Number=Plur|PronType=Art	2	det	_	_\n'
+        '2	études	étude	NOUN	_	Gender=Fem|Number=Plur	3	nsubj	_	_\n'
+        '3	durent	durer	VERB	_	Mood=Ind|Number=Plur|Person=3|Tense=Pres|VerbForm=Fin	0	root	_	_\n'
+        '4	six	six	NUM	_	_	5	nummod	_	_\n'
+        '5	ans	an	NOUN	_	Gender=Masc|Number=Plur	3	obj	_	_\n'
+        '6	mais	mais	CCONJ	_	_	9	cc	_	_\n'
+        '7	leur	son	DET	_	Gender=Masc|Number=Sing|Poss=Yes|PronType=Prs	8	det	_	_\n'
+        '8	contenu	contenu	NOUN	_	Gender=Masc|Number=Sing	9	nsubj	_	_\n'
+        '9	diffère	différer	VERB	_	Mood=Ind|Number=Sing|Person=3|Tense=Pres|VerbForm=Fin	3	conj	_	_\n'
+        '10	donc	donc	ADV	_	_	9	advmod	_	_\n'
+        '11	selon	selon	ADP	_	_	13	case	_	_\n'
+        '12	les	le	DET	_	Definite=Def|Number=Plur|PronType=Art	13	det	_	_\n'
+        '13	Facultés	facultés	NOUN	_	Number=Fem	9	obl	_	SpaceAfter=No\n'
+        '14	.	.	PUNCT	_	_	3	punct	_	_')
 
-    output = ('# sent_id = fr-ud-dev_00231\n'
-              '# text = Les études durent six ans mais leur contenu diffère donc selon les Facultés.\n'
-              '1	Les	le	DET	_	Definite=Def|Gender=Fem|Number=Plur|PronType=Art	2	det	_	_\n'
-              '2	études	étude	NOUN	_	Gender=Fem|Number=Plur	3	nsubj	_	_\n'
-              '3	durent	durer	VERB	_	Mood=Ind|Number=Plur|Person=3|Tense=Pres|VerbForm=Fin	0	root	_	_\n'
-              '4	six	six	NUM	_	_	5	nummod	_	_\n'
-              '5	ans	an	NOUN	_	Gender=Masc|Number=Plur	3	obj	_	_\n'
-              '6	mais	mais	CCONJ	_	_	9	cc	_	_\n'
-              '7	leur	son	DET	_	Gender=Masc|Number=Sing|Poss=Yes|PronType=Prs	8	det	_	_\n'
-              '8	contenu	contenu	NOUN	_	Gender=Masc|Number=Sing	9	nsubj	_	_\n'
-              '9	diffère	différer	VERB	_	Mood=Ind|Number=Sing|Person=3|Tense=Pres|VerbForm=Fin	3	conj	_	_\n'
-              '10	donc	donc	ADV	_	_	9	advmod	_	_\n'
-              '11	selon	selon	ADP	_	_	13	case	_	_\n'
-              '12	les	le	DET	_	Definite=Def|Number=Plur|PronType=Art	13	det	_	_\n'
-              '13	Facultés	facultés	NOUN	_	Number=Fem	9	obl	_	SpaceAfter=No\n'
-              '14	.	.	PUNCT	_	_	3	punct	_	_')
-
-    assert sentence.conllu() == output 
+    assert sentence.conllu() == output
