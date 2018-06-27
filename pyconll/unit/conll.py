52377ad8b4cb760286d98f4df3e192df19ea52fb
Matias Grioni <matgrioni@gmail.com>
2018-06-26 22:42:25 -0700

    Remove duplicate methods.

--- pyconll/unit/conll.py
+++ pyconll/unit/conll.py
@@ -62,37 +62,6 @@ class Conll:
             writable.write(sentence.conll())
             writable.write('\n\n')
 
-    def conll(self):
-        """
-        Output the Conll object to a CoNLL-U formatted string.
-
-        Returns:
-        The CoNLL-U object as a string. This string will end in a newline.
-        """
-        # Add newlines along with sentence strings so that there is no need to
-        # slice potentially long lists or modify strings.
-        components = []
-        for sentence in self._sentences:
-            components.append(sentence.conll())
-            components.append('\n\n')
-
-        return ''.join(components)
-
-    def write(self, writable):
-        """
-        Write the Conll object to something that is writable.
-
-        For simply writing, this method is more efficient than calling conll
-        then writing since no string of the entire Conll object is created. The
-        final output will include a final newline.
-
-        Args:
-        writable: The writable object such as a file. Must have a write method.
-        """
-        for sentence in self._sentences:
-            writable.write(sentence.conll())
-            writable.write('\n\n')
-
     def __iter__(self):
         """
         Allows for iteration over every sentence in the CoNLL-U file.
