472c7c818510ace039c7fd6d5987e559ed154b58
Matias Grioni <matgrioni@gmail.com>
2018-07-20 20:06:54 -0700

    Back again.

--- docs/conf.py
+++ docs/conf.py
@@ -47,7 +47,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.coverage',
     'sphinx.ext.viewcode',
-    'sphinxcontrib.napoleon'
+    'sphinx.ext.napoleon'
 ]
 
 napoleon_google_docstring = True
