3f5f1cb37b77db0e861a5bb2b420ad604d6545b1
Matias Grioni <matgrioni@gmail.com>
2018-07-20 19:42:02 -0700

    Fix docstring presentation on docs.

--- docs/conf.py
+++ docs/conf.py
@@ -47,6 +47,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.coverage',
     'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
