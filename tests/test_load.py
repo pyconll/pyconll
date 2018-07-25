5c99e3a9dbbac6e57c0a3d0a439a0da8e5c40f53
Matias Grioni <matgrioni@gmail.com>
2018-07-24 22:14:08 -0700

    Add ability to load CoNLL from network.

--- tests/test_load.py
+++ tests/test_load.py
@@ -1,4 +1,9 @@
-from pyconll import load_from_string, load_from_file, iter_from_string, iter_from_file
+import requests
+import responses
+
+import pytest
+
+from pyconll import load_from_string, load_from_file, load_from_url, iter_from_string, iter_from_file, iter_from_url
 from tests.util import fixture_location
 from tests.unit.util import assert_token_members
 
@@ -30,6 +35,36 @@ def test_load_from_file():
     assert sent['10'].form == 'donc'
 
 
+@responses.activate
+def test_load_from_url():
+    """
+    Test that a CoNLL can be properly loaded from a url.
+    """
+    TEST_CONLL_URL = 'https://myconllrepo.com/english/train'
+    with open(fixture_location('basic.conll')) as f:
+        responses.add(responses.GET, TEST_CONLL_URL, body=f.read())
+
+    c = load_from_url(TEST_CONLL_URL)
+    sent = c[1]
+
+    assert len(c) == 4
+    assert len(sent) == 14
+    assert sent['10'].form == 'donc'
+
+
+@responses.activate
+def test_load_from_url_fail():
+    """
+    Test that the correct error is thrown when there is a load error.
+    """
+    TEST_CONLL_URL = 'https://myconllrepo.com/english/train'
+    WRONG_URL = 'https://myconllrepo.com/english/wrong'
+    responses.add(responses.GET, TEST_CONLL_URL)
+
+    with pytest.raises(requests.exceptions.RequestException):
+        c = load_from_url(WRONG_URL)
+
+
 def test_load_from_file_and_string_equivalence():
     """
     Test that the Conll object created from a string and file is the same if
@@ -50,6 +85,36 @@ def test_load_from_file_and_string_equivalence():
         for str_token in str_c[i]:
             file_token = file_c[i][str_token.id]
             assert_token_members(str_token, file_token.id, file_token.form,
+                                 file_token.lemma, file_token.upos,
+                                 file_token.xpos, file_token.feats,
+                                 file_token.head, file_token.deprel,
+                                 file_token.deps, file_token.misc)
+
+
+@responses.activate
+def test_load_from_file_and_url_equivalence():
+    """
+    Test that the Conll object created from a string and file is the same if
+    the underlying source is the same.
+    """
+    TEST_CONLL_URL = 'https://myconllrepo.com/english/train'
+    with open(fixture_location('long.conll')) as f:
+        contents = f.read()
+        responses.add(responses.GET, TEST_CONLL_URL, body=contents)
+
+    url_c = load_from_url(TEST_CONLL_URL)
+    file_c = load_from_file(fixture_location('long.conll'))
+
+    assert len(url_c) == len(file_c)
+    for i in range(len(url_c)):
+        assert url_c[i].id == file_c[i].id
+        assert url_c[i].text == file_c[i].text
+        print(url_c[i].conll())
+        print(file_c[i].conll())
+
+        for url_token in url_c[i]:
+            file_token = file_c[i][url_token.id]
+            assert_token_members(url_token, file_token.id, file_token.form,
                                  file_token.lemma, file_token.upos,
                                  file_token.xpos, file_token.feats,
                                  file_token.head, file_token.deprel,
@@ -80,3 +145,33 @@ def test_iter_from_file():
     ]
 
     assert expected_ids == actual_ids
+
+
+@responses.activate
+def test_iter_from_network():
+    """
+    Test that a CoNLL file over a network can be iterated.
+    """
+    TEST_CONLL_URL = 'https://myconllrepo.com/english/train'
+    with open(fixture_location('basic.conll')) as f:
+        responses.add(responses.GET, TEST_CONLL_URL, body=f.read())
+
+    expected_ids = ['fr-ud-dev_0000{}'.format(i) for i in range(1, 5)]
+    actual_ids = [sent.id for sent in iter_from_url(TEST_CONLL_URL)]
+
+    assert expected_ids == actual_ids
+
+
+@responses.activate
+def test_iter_from_network():
+    """
+    Test that a CoNLL file over a network can be iterated.
+    """
+    TEST_CONLL_URL = 'https://myconllrepo.com/english/train'
+    WRONG_URL = 'https://myconllrepo.com/english/gibberish'
+    with open(fixture_location('basic.conll')) as f:
+        responses.add(responses.GET, TEST_CONLL_URL, body=f.read())
+
+    with pytest.raises(requests.exceptions.RequestException):
+        for sent in iter_from_url(WRONG_URL):
+            pass
