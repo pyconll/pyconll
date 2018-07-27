62d49ef8116013d50b948ae312b918f6b4d0a61d
Matias Grioni <matgrioni@gmail.com>
2018-07-26 23:31:06 -0700

    Add some validation and test to sentence.

--- tests/unit/test_sentence.py
+++ tests/unit/test_sentence.py
@@ -948,3 +948,30 @@ def test_no_text_singleton():
     sentence = Sentence(source)
 
     assert sentence.text is None
+
+
+def test_invalid_sentence_by_token():
+    """
+    Test that an invalid token results in an invalid sentence.
+    """
+    source = (
+        '# newpar id\n'
+        '# sent_id =\n'
+        '# text =\n'
+        '1	Les	le	DET	_	Definite=Def|Gender=Fem|Number=Plur|PronType=Art	2	det	_	_\n'
+        '2	études	étude	NOUN	_	Gender=Fem|Number=Plur	3	nsubj	_	_\n'
+        '3	durent	durer	VERB	_	Mood=Ind|Number=Plur|Person=3|Tense=Pres|VerbForm=Fin	0	root	_	_\n'
+        '4	six	six	NUM	_	_	5	nummod	_	_\n'
+        '5	ans	an	NOUN	_	Gender=Masc|Number=Plur	3	obj	_	_\n'
+        '6	mais	mais	CCONJ	_	_	9	cc	_	_\n'
+        '7	leur	son	DET	_	Gender|Number=Sing|Poss=Yes|PronType=Prs	8	det	_	_\n'
+        '8	contenu	contenu	NOUN	_	Gender=Masc|Number=Sing	9	nsubj	_	_\n'
+        '9	diffère	différer	VERB	_	Mood=Ind|Number=Sing|Person=3|Tense=Pres|VerbForm=Fin	3	conj	_	_\n'
+        '10	donc	donc	ADV	_	_	9	advmod	_	_\n'
+        '11	selon	selon	ADP	_	_	13	case	_	_\n'
+        '12	les	le	DET	_	Definite=Def|Number=Plur|PronType=Art	13	det	_	_\n'
+        '13	Facultés	Facultés	PROPN	_	_	9	obl	_	SpaceAfter=No\n'
+        '14	.	.	PUNCT	_	_	3	punct	_	_')
+
+    with pytest.raises(ValueError):
+        sentence = Sentence(source)
