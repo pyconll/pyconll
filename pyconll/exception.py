e2862ed4b218fe44455c700678811918305bf5c6
Matias Grioni <matgrioni@gmail.com>
2018-09-13 21:20:55 -0700

    Update tests nad fix problems with formatting.

--- pyconll/exception.py
+++ pyconll/exception.py
@@ -3,3 +3,10 @@ class ParseError(ValueError):
     Error that results from an improper value into a parsing routine.
     """
     pass
+
+
+class FormatError(ValueError):
+    """
+    Error that results from trying to format a CoNLL object to a string.
+    """
+    pass
