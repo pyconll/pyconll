95d0ac0b71c68c9fe25da87958bbfbaf947f8138
Matias Grioni <matgrioni@gmail.com>
2018-10-04 22:01:13 -0700

    Might as well fix version number to match next version.

--- docs/conf.py
+++ docs/conf.py
@@ -29,9 +29,9 @@ copyright = '2018, Matias Grioni'
 author = 'Matias Grioni'
 
 # The short X.Y version
-version = '1.0.1'
+version = '1.0.2'
 # The full version, including alpha/beta/rc tags
-release = '1.0.1'
+release = '1.0.2'
 
 
 # -- General configuration ---------------------------------------------------
