0246ffbe8c999e3126111bee6705793343f54cef
Matias Grioni <matgrioni@gmail.com>
2018-11-11 17:04:45 -0800

    Add test for conllable.

---
+++ tests/test_conllable.py
@@ -0,0 +1,12 @@
+import pytest
+
+from pyconll.conllable import Conllable
+
+def test_conllable_throws_exception():
+    """
+    Test that the base Conllable implementation throws an exception.
+    """
+    c = Conllable()
+
+    with pytest.raises(NotImplementedError):
+        c.conll()
