e778869c4eab2475c35c42c5fdc720aeda4e9669
Matias Grioni <matgrioni@gmail.com>
2019-02-03 20:11:43 -0500

    Mostly finish and document TreeBuilder and update tests.

--- tests/tree/test_tree.py
+++ tests/tree/test_tree.py
@@ -1,16 +1,15 @@
 import pytest
 
-from pyconll.tree import Tree
+from pyconll.tree import Tree, TreeBuilder
 
 
 def test_empty_tree_creation():
     """
     Test that an empty tree with no data and no children.
     """
-    t = Tree(None, None)
+    t = Tree()
 
     assert len(t) == 0
-    assert t.children == []
     assert t.data is None
 
 
@@ -18,10 +17,7 @@ def test_children_read_only():
     """
     Test that children on a Tree cannot be assigned after construction.
     """
-    child1 = Tree(2, None)
-    child2 = Tree(7, None)
-    t = Tree(1, [child1])
-
+    t = Tree()
     with pytest.raises(AttributeError):
         t.children = []
 
@@ -30,37 +26,38 @@ def test_parent_read_only():
     """
     Test that the parent field cannot be assigned after construction.
     """
-    child1 = Tree(2, None)
-    child2 = Tree(7, None)
-    t = Tree(1, [child1, child2])
-
+    t = Tree()
     with pytest.raises(AttributeError):
-        t[0].parent = None
+        t.parent = None
 
 
 def test_get_children():
     """
     Test that we can properly retreive children from a tree by index.
     """
-    child11 = Tree(13, None)
-    child1 = Tree(2, [child11])
-    child2 = Tree(7, None)
+    builder = TreeBuilder()
+    builder.create_root(1)
+    builder.add_child(7)
+    builder.add_child(2, move=True)
+    builder.add_child(13)
+    t = builder.build()
 
-    t = Tree(1, [child1, child2])
-
-    assert t[0] == child1
-    assert t[1] == child2
-    assert t[0][0] == child11
+    assert t[0].data == 7
+    assert t[1].data == 2
+    assert t[1][0].data == 13
 
 
 def test_iter_children():
     """
     Test that we can properly iterate over the children of a tree.
     """
-    data = list(range(2, 15, 3))
-    children = list(map(lambda el: Tree(el, None), data))
+    builder = TreeBuilder()
+    builder.create_root()
 
-    t = Tree(0, children)
+    data = list(range(2, 15, 3))
+    for datum in data:
+        builder.add_child(datum)
+    t = builder.build()
 
     for i, child in enumerate(t):
         assert child.data == data[i]
@@ -70,11 +67,19 @@ def test_len_children():
     """
     Test that we can properly get the number of children.
     """
-    data = list(range(2, 15, 3))
-    subdata = list(map(lambda el: Tree(el, None), [0, 1, 2]))
-    children = list(map(lambda el: Tree(el, subdata), data))
+    builder = TreeBuilder()
+    builder.create_root()
 
-    t = Tree(0, children)
+    data = list(range(2, 15, 3))
+    subdata = [0, 1, 2, 3, 4]
+    for datum in data:
+        builder.add_child(datum, move=True)
+
+        for subdatum in subdata:
+            builder.add_child(subdatum)
+
+        builder.move_to_parent()
+    t = builder.build()
 
     assert len(t) == len(data)
     for child in t:
@@ -85,13 +90,16 @@ def test_parent_assigment():
     """
     Test that children tree's parents are properly assigned.
     """
-    child11 = Tree(13, None)
-    child1 = Tree(2, [child11])
-    child2 = Tree(7, None)
+    builder = TreeBuilder()
+    builder.create_root()
+    builder.add_child(2, move=True)
+    builder.add_child(13)
+    builder.move_to_parent()
+    builder.add_child(7)
 
-    t = Tree(1, [child1, child2])
+    t = builder.build()
 
     assert t.parent is None
-    assert child1.parent == t
-    assert child2.parent == t
-    assert child11.parent == child1
+    assert t[0].parent == t
+    assert t[1].parent == t
+    assert t[0][0].parent == t[0]
