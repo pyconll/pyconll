1b169bb7d43031cd61a026eeac012b838ced19a7
Matias Grioni <matgrioni@gmail.com>
2019-04-16 21:44:51 -0700

    An initial approach to the Tree API, with possible issues.

--- pyconll/tree/__init__.py
+++ pyconll/tree/__init__.py
@@ -1,9 +1,5 @@
 """
-Defines the utilties for interfacing with CoNLL sentences as trees. This
-is a utility method which when provided a Sentence constructs the appropriate 
+Defines the modules for interfacing with CoNLL sentences as trees. This
+is a utility module which when provided a Sentence constructs the appropriate 
 or corresponding tree structure.
 """
-
-__all__ = ['sentencetree']
-
-from .sentencetree import SentenceTree
