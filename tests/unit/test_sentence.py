e3417862ad26e5bd61fa4192a843010c1099eef8
Matias Grioni <matgrioni@gmail.com>
2019-04-30 22:21:15 -0700

    Add better test coverage and code deduplication.

--- tests/unit/test_sentence.py
+++ tests/unit/test_sentence.py
@@ -1,6 +1,8 @@
 import pytest
 
 from pyconll.unit import Sentence
+
+from tests.tree.util import assert_tree_structure
 from tests.unit.util import assert_token_members
 
 
@@ -472,6 +474,33 @@ def test_int_slice_indexing_missing_value_stop():
                          {'SpaceAfter': set(('No', ))})
     assert_token_members(test_tokens[3], '14', '.', '.', 'PUNCT', None, {},
                          '3', 'punct', {}, {})
+
+
+def test_proper_slice_type():
+    """
+    Test that the type provided to a slice must be an int, str, or slice.
+    """
+    source = (
+        '# sent_id = fr-ud-dev_00002\n'
+        '# text = Les études durent six ans mais leur contenu diffère donc selon les Facultés.\n'
+        '1	Les	le	DET	_	Definite=Def|Gender=Fem|Number=Plur|PronType=Art	2	det	_	_\n'
+        '2	études	étude	NOUN	_	Gender=Fem|Number=Plur	3	nsubj	_	_\n'
+        '3	durent	durer	VERB	_	Mood=Ind|Number=Plur|Person=3|Tense=Pres|VerbForm=Fin	0	root	_	_\n'
+        '4	six	six	NUM	_	_	5	nummod	_	_\n'
+        '5	ans	an	NOUN	_	Gender=Masc|Number=Plur	3	obj	_	_\n'
+        '6	mais	mais	CCONJ	_	_	9	cc	_	_\n'
+        '7	leur	son	DET	_	Gender=Masc|Number=Sing|Poss=Yes|PronType=Prs	8	det	_	_\n'
+        '8	contenu	contenu	NOUN	_	Gender=Masc|Number=Sing	9	nsubj	_	_\n'
+        '9	diffère	différer	VERB	_	Mood=Ind|Number=Sing|Person=3|Tense=Pres|VerbForm=Fin	3	conj	_	_\n'
+        '10	donc	donc	ADV	_	_	9	advmod	_	_\n'
+        '11	selon	selon	ADP	_	_	13	case	_	_\n'
+        '12	les	le	DET	_	Definite=Def|Number=Plur|PronType=Art	13	det	_	_\n'
+        '13	Facultés	Facultés	PROPN	_	_	9	obl	_	SpaceAfter=No\n'
+        '14	.	.	PUNCT	_	_	3	punct	_	_')
+    sentence = Sentence(source)
+
+    with pytest.raises(ValueError):
+        token = sentence[7.8]
 
 
 def test_len_basic():
@@ -949,26 +978,6 @@ def test_invalid_sentence_by_token():
         sentence = Sentence(source)
 
 
-def _assert_tree_structure(sentence, tree, root_token, children_paths):
-    """
-    Assert a tree structure from tree node paths to sentence tokens.
-
-    Args:
-        sentence: The sentence that the tree is created from.
-        tree: The tree whose structure to inspect.
-        root_token: The root token of the tree.
-        children_paths: A dictionary from child indices to token indices.
-    """
-    assert tree.data == root_token
-
-    for path, token_key in children_paths.items():
-        cur = tree
-        for component in path:
-            cur = cur[component]
-
-        assert cur.data == sentence[token_key]
-
-
 def test_to_tree_standard_sentence():
     """
     Test that a normal sentence can be parsed properly.
@@ -982,10 +991,11 @@ def test_to_tree_standard_sentence():
     sentence = Sentence(source)
     st = sentence.to_tree()
 
-    _assert_tree_structure(sentence, st, sentence[2], {
-        (0, ): 0,
-        (1, ): 1,
-        (2, ): 3
+    assert_tree_structure(st, {
+        (): sentence[2],
+        (0, ): sentence[0],
+        (1, ): sentence[1],
+        (2, ): sentence[3]
     })
 
 
@@ -1013,21 +1023,22 @@ def test_to_tree_multi_level():
     sentence = Sentence(source)
     st = sentence.to_tree()
 
-    _assert_tree_structure(
-        sentence, st, sentence[2], {
-            (0, ): 1,
-            (1, ): 4,
-            (2, ): 8,
-            (3, ): 13,
-            (0, 0): 0,
-            (1, 0): 3,
-            (2, 0): 5,
-            (2, 1): 7,
-            (2, 2): 9,
-            (2, 3): 12,
-            (2, 1, 0): 6,
-            (2, 3, 0): 10,
-            (2, 3, 1): 11
+    assert_tree_structure(
+        st, {
+            (): sentence[2],
+            (0, ): sentence[1],
+            (1, ): sentence[4],
+            (2, ): sentence[8],
+            (3, ): sentence[13],
+            (0, 0): sentence[0],
+            (1, 0): sentence[3],
+            (2, 0): sentence[5],
+            (2, 1): sentence[7],
+            (2, 2): sentence[9],
+            (2, 3): sentence[12],
+            (2, 1, 0): sentence[6],
+            (2, 3, 0): sentence[10],
+            (2, 3, 1): sentence[11]
         })
 
 
