ec1bfd1a73919e4f7154d4f0edcd525e99eafe29
Matias Grioni <matgrioni@gmail.com>
2020-05-10 12:40:28 -0700

    Update comment to express adding as well.

--- pyconll/unit/sentence.py
+++ pyconll/unit/sentence.py
@@ -150,7 +150,7 @@ class Sentence(Conllable):
 
     def set_meta(self, key, value=None):
         """
-        Set the metadata or comments associated with this Sentence.
+        Set or add the metadata or comments associated with this Sentence.
 
         Args:
             key: The key for the comment.
