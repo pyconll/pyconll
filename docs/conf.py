85860090f27504a7d78fa63659e789e9240d961f
Matias Grioni <matgrioni@gmail.com>
2021-01-14 23:24:16 -0800

    Omit icon for now.

--- docs/conf.py
+++ docs/conf.py
@@ -98,7 +98,7 @@ pygments_style = 'sphinx'
 html_theme = 'default'
 
 # Set the path for the icon
-html_favicon = 'icon.png'
+# html_favicon = 'icon.png'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
