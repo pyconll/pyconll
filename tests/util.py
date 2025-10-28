aaba71790ec58445a979848046ebfda93336c494
Matias Grioni <matgrioni@gmail.com>
2025-10-27 23:12:15 -0400

    Initial Implementation of Structural Parsing (#146)
    * Step 1 via cc. Change parsing logic into a class.
    
    * Move parsing out of Token class.
    
    * Lint fixes.
    
    * Move everything up to 3.12 and small start on structural schema logic.
    
    * Some wip.
    
    * Attempt before reformat.
    
    * Update formatting logic properly.
    
    * Pretty good progress I'd say.
    
    * MFer, it works.
    
    * Still need to do more hardening with handling and surfacing errors, but it works for ConlluToken unit and int tests.
    
    * Getting a move on.
    
    * Fix version number and add more to CHANGELOG.
    
    * Working through it.
    
    * More implementation improvements.
    
    * Handle more errors and inconsistencies, improve error handling distinctions, etc.
    
    * Annoying string reformatting.
    
    * More improvements.
    
    * Fix all tests, make an opt-out for all the file hashing, and add a __repr__ for the generated classes.
    
    * Work on restructuring modules.
    
    * Proper annotation of public API, and fix ABC issue on Conllable so that type will be able to be piped in from top level.
    
    * Revert load API to avoid breaking change, and change __init__ imports, and add custom schema descriptor.
    
    * Small linting changes.
    
    * Change minimum required version to 3.13 and start moving around code for parsing consolidation.
    
    * Get mypy to shut-it.
    
    * Update loader.
    
    * Change tests back.
    
    * Move back and consider consolidated and time to bring up quality before next checkpoint.
    
    * Add new command.
    
    * Update versions again.
    
    * Big linting changes.
    
    * Rename to better match concepts and fix syntax error.
    
    * Remove legacy parsing.
    
    * Almost there.
    
    * More safe name generation.
    
    * Move up python version to take advantage of template strings and increase safety.
    
    * Change things to t-strings for safety and nail down the design finally.
    
    * Try to address linting errors.
    
    * Maybe python 3.14 was a mistake :).
    
    * Update documentation build version.
    
    * Add tests, small optimization, and fix line length and bug.
    
    * Resign to the fact that Parse and FormatError should only really be thrown for Token parsing.
    
    * Continue being a boss.
    
    * Fix linting and test coverage.
    
    * Fix formatting.

--- tests/util.py
+++ tests/util.py
@@ -5,4 +5,4 @@ def fixture_location(name):
     """
     Get the file location of the fixture with the given name.
     """
-    return os.path.join(os.path.dirname(__file__), 'fixtures', name)
+    return os.path.join(os.path.dirname(__file__), "fixtures", name)
