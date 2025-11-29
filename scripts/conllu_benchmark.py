931c12a143464807dd74806756b5ed66b246f8e3
Matias Grioni <matgrioni@gmail.com>
2025-11-29 13:33:14 -0800

    Final progress on benchmark.

--- scripts/conllu_benchmark.py
+++ scripts/conllu_benchmark.py
@@ -5,9 +5,7 @@ from enum import auto, Enum
 import gc
 import hashlib
 import logging
-import os
 from pathlib import Path
-import psutil
 import sys
 import time
 from typing import Callable
@@ -16,8 +14,7 @@ import conllu as alt
 
 sys.path.insert(0, str(Path(__file__).parent.parent))
 
-from pyconll.conllu import compact_conllu, conllu, Token
-from pyconll.format import Format
+from pyconll.conllu import compact_conllu, conllu
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
@@ -28,21 +25,27 @@ class ParserType(Enum):
     ALTERNATIVE = auto()
 
 
+class MeasurementType(Enum):
+    RUNTIME = auto()
+    MEMORY = auto()
+
+
+class ReportingUnit(Enum):
+    RAW = auto()
+    KB_PER = auto()
+    PER_KB = auto()
+
+
 @dataclass
 class Args:
     corpora: Path
-    parser: Format[Token]
+    parser: ParserType
+    measurement: MeasurementType
+    reporting_unit: ReportingUnit
     loops_per_file: int
     total_loops: int
     coverage: float
     output_csv: Path
-
-
-@dataclass
-class ConlluBenchmark:
-    duration: float
-    file_size: int
-    memory: int
 
 
 EXCLUSIONS_BY_PARSER: dict[ParserType, set[str]] = {
@@ -50,19 +53,14 @@ EXCLUSIONS_BY_PARSER: dict[ParserType, set[str]] = {
 }
 
 
-def _get_current_memory_usage() -> int:
-    p = psutil.Process(os.getpid())
-    memory_info = p.memory_info()
-    return memory_info.rss
-
-
 def kernel(
     files: list[Path],
     loops_per_file: int,
     parser: Callable[[str], list],
+    measure: Callable[[str, Callable[[str], list]], float],
     coverage: float,
     exclusions: set[str],
-) -> dict[str, ConlluBenchmark]:
+) -> dict[Path, list[float]]:
     results = {}
     for file in files:
         hasher = hashlib.blake2b(str(file).encode(), digest_size=8)
@@ -73,38 +71,22 @@ def kernel(
 
         if file.name in exclusions:
             logging.info("Skipping %s because it is excluded for this parser.", file)
-            results[file.stem] = ConlluBenchmark(-1, -1, -1)
+            results[file.stem] = []
             continue
 
         text = file.read_text(encoding="utf-8")
 
-        duration = 0.0
-        logging.info("Starting runtime performance measuring loop for %s", file)
+        values: list[float] = []
+        logging.info("Starting measurement loop for %s", file)
         for i in range(loops_per_file):
-            logging.info("Runtime performance measurement loop number %d for %s", i + 1, file)
-            start = time.perf_counter()
-            corpus = parser(text)
-            end = time.perf_counter()
-
-            del corpus
+            logging.info("Starting iteration %d for %s", i + 1, file)
+            value = measure(text, parser)
             gc.collect()
-
-            duration += end - start
-
-        logging.info("Starting memory usage measurement for %s", file)
-        intial_rss_bytes = _get_current_memory_usage()
-        corpus = parser(text)
-        post_rss_bytes = _get_current_memory_usage()
-
-        # Because parsing can be very high memory usage, make sure to cleanup all memory used
-        # between invocations.
-        del corpus
-        del text
-        gc.collect()
+            values.append(value)
 
         logging.info("Finished measurements for %s", file)
 
-        results[file.stem] = ConlluBenchmark(duration, file.stat().st_size * loops_per_file, post_rss_bytes - intial_rss_bytes)
+        results[file] = values
 
     return results
 
@@ -120,27 +102,69 @@ def main(args: Args) -> None:
     else:
         raise RuntimeError(f"{args.parser} is not a properly handled format type.")
 
+    measure: Callable[[str, Callable[[str], list]], float]
+    if args.measurement == MeasurementType.RUNTIME:
+        def measure(text: str, parse: Callable[[str], list]) -> float:
+            start = time.perf_counter()
+            corpus = parse(text)
+            end = time.perf_counter()
+
+            del corpus
+
+            return end - start
+    elif args.measurement == MeasurementType.MEMORY:
+        def measure(text: str, parse: Callable[[str], list]) -> float:
+            from guppy import hpy
+            hp = hpy()
+            hp.setrelheap()
+            corpus = parse(text)
+            h = hp.heap()
+            memory = h.size
+
+            del h
+            del corpus
+            del hp
+            del hpy
+
+            return memory
+
+    else:
+        raise RuntimeError(f"{args.measurement} is not a properly handled measurement type.")
+
+    convert: Callable[[Path, float], float]
+    if args.reporting_unit == ReportingUnit.RAW:
+        convert = lambda _, value: value
+    elif args.reporting_unit == ReportingUnit.KB_PER:
+        convert = lambda p, value: (p.stat().st_size / 1024) / value
+    elif args.reporting_unit == ReportingUnit.PER_KB:
+        convert = lambda p, value: value / (p.stat().st_size / 1024)
+    else:
+        raise RuntimeError(f"{args.reporting_unit} is not a properly handled reporting unit type.")
+
     files = sorted(args.corpora.glob("**/*.conllu"), key=lambda p: p.stat().st_size, reverse=True)
     results = kernel(
         files,
         args.loops_per_file,
         parser,
+        measure,
         args.coverage,
         EXCLUSIONS_BY_PARSER.get(args.parser, set()),
     )
 
     with open(args.output_csv, "w", encoding="utf-8") as f:
-        writer = csv.DictWriter(f, ["key", "kbps", "in_memory_kb", "disk_kb"])
+        measurement_keys = [f"measurement_{i}" for i in range(args.loops_per_file)]
+        writer = csv.DictWriter(f, ["key", "disk_kb", *measurement_keys])
         writer.writeheader()
 
-        for conllu_key, benchmark in sorted(results.items(), key=lambda p: p[0]):
-            bps = benchmark.file_size / benchmark.duration
+        for conllu_file, values in sorted(results.items(), key=lambda p: p[0]):
+            disk_kb = conllu_file.stat().st_size / 1024
+            cols = { f"measurement_{i}": convert(conllu_file, v) for i, v in enumerate(values) }
+
             writer.writerow(
                 {
-                    "key": conllu_key,
-                    "kbps": bps / 1024,
-                    "in_memory_kb": benchmark.memory / 1024,
-                    "disk_kb": benchmark.file_size / 1024,
+                    "key": conllu_file.stem,
+                    "disk_kb": disk_kb,
+                    **cols
                 }
             )
 
@@ -160,6 +184,12 @@ if __name__ == "__main__":
         help="The type of CoNLL-U parser to use.",
     )
     parser.add_argument(
+        "--measurement",
+        type=lambda s: MeasurementType[s],
+        required=True,
+        help="The type of measurement to make on the parsing."
+    )
+    parser.add_argument(
         "--loops_per_file", type=int, required=True, help="The number of times to parse each file"
     )
     parser.add_argument(
@@ -177,6 +207,12 @@ if __name__ == "__main__":
         ),
     )
     parser.add_argument(
+        "--reporting_unit",
+        type=lambda s: ReportingUnit[s],
+        required=True,
+        help="The way to choose to display the measurement value in the final results."
+    )
+    parser.add_argument(
         "--output_csv", type=Path, required=True, help="The location to write the csv output to."
     )
 
@@ -186,6 +222,8 @@ if __name__ == "__main__":
         Args(
             args.corpora,
             args.parser,
+            args.measurement,
+            args.reporting_unit,
             args.loops_per_file,
             args.total_loops,
             args.coverage,
